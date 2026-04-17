539aa5577099820271ca5a638d50f612142503c4
Mahad Abdullahi Ahmed <mahad.ahmed613@gmail.com>
2026-04-16 22:19:05 -0400

    added build sim script

---
+++ build_sim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$CADMIUM" ]; then
+    echo "ERROR: CADMIUM environment variable is not set"
+    echo "Please set it to your Cadmium v2 include directory:"
+    echo "export CADMIUM=/path/to/cadmium_v2/include"
+    exit 1
+fi
+
+if [ -d "build" ]; then rm -Rf build; fi
+if [ -d "bin" ]; then rm -Rf bin; fi
+mkdir -p build
+mkdir -p bin
+mkdir -p simulation_results
+make clean
+make all
+echo "Compilation done. Executables in the bin folder"
